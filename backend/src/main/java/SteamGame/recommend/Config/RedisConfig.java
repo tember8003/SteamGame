c302ba1e5e083b871ec08f9b6b6c8082fc3c6f54
yuchan <yuchan8003@naver.com>
2025-04-17 21:58:49 +0900

    Feat:Redis에 추천된 게임 추가 + Exception 설정

--- backend/src/main/java/SteamGame/recommend/Config/RedisConfig.java
+++ backend/src/main/java/SteamGame/recommend/Config/RedisConfig.java
@@ -10,8 +10,8 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 @Configuration
 public class RedisConfig {
     @Bean
-    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory connectionFactory){
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
+    public RedisTemplate<String,Boolean> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<String, Boolean> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
--- backend/src/main/java/SteamGame/recommend/Config/WebClientConfig.java
+++ backend/src/main/java/SteamGame/recommend/Config/WebClientConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
+//현재 사용 X
 @Configuration
 public class WebClientConfig {
 
